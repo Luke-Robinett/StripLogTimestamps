8c71de8831ef8702948f84474679c1efb5afc5a5
Luke Robinett <LRobinett@foxen.com>
2023-01-25 18:40:02 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,8 @@
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault();
+    
+    const inputText = document.querySelector("#inputTextArea").value;
+    const outputTextArea = document.querySelector("#outputTextArea");
+    
+    const rx = new RegExp();
+});
